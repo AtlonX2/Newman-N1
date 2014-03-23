6803a8b815340870dc179838a9aee31c1a17cfec
lingfude <ling.fude@gmail.com>
2014-03-23 22:17:36 +0000

    Initial commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 55 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#55 SMP PREEMPT Sat Mar 22 23:21:38 WET 2014"
+#define LINUX_COMPILE_BY "david"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ kernel/usr/.initramfs_data.cpio.d
@@ -0,0 +1,1 @@
+deps_initramfs := /home/david/Android/Newman-N1/kernel/scripts/gen_initramfs_list.sh \
