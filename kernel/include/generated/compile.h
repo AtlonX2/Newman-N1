971c864f3e5387c7ebac473f8033ee73948d45e6
lingfude <ling.fude@gmail.com>
2014-03-23 23:27:15 +0000

    Initial commit

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 56 */
+/* This file is auto generated, version 57 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#56 SMP PREEMPT Sun Mar 23 22:53:21 WET 2014"
+#define UTS_VERSION "#57 SMP PREEMPT Sun Mar 23 23:13:40 WET 2014"
 #define LINUX_COMPILE_BY "david"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
